763d07552963abfb2194240c74a1c414ae91a419
Richard Cole Tumolac <rctumolac@gmail.com>
2017-01-10 05:23:55 +0000

    Changed about page and added a new CSS file

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+/* global $ */
 $(document).ready(function(){
     //Write all your jQuery code here
-        $("button").click(function() {
-      $("#like").html("You like this project!");
+    $("button").click(function() {
+    $("#like").html("You like this project!");
     });
     
-    
-    
-    
+$("#home").hover(function(){
+   $("#home").css("background-color", "#D3D3D3");
+},
+    function(){
+         $("#home")
+    }
+);
+
 });
+    
